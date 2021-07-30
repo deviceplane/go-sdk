975c09f342ac64844e0e0a00b7df7f095b1e5477
sergerad <serge.radinovich@gmail.com>
2021-07-30 14:31:57 +1200

    Update feature tests

--- test/rest/client/client_test.go
+++ test/rest/client/client_test.go
@@ -15,21 +15,27 @@ var (
 	testClient *client.Client
 	password   string
 	email      string
+	project   string
+	projectID  string
 )
 
 const (
 	company   = "Edgeworx"
 	firstName = "Test"
 	lastName  = "Testovich"
-	project   = "testproject"
+	roleYAML = `rules:
+- resources:
+  - '*'
+  actions:
+  - write`
 )
 
 func randomString(n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyz")
-
+     
 	s := make([]rune, n)
 	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
+	    s[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(s)
 }
@@ -50,28 +56,37 @@ func TestNew(t *testing.T) {
 	}
 }
 
-func TestCreateUser(t *testing.T) {
-	email = fmt.Sprintf("gotest%s@testworx.io", randomString(7))
-	password = os.Getenv("USER_PW")
-	if password == "" {
-		t.Fatal("Environment variable USER_PW is not set")
-	}
-	user, err := testClient.CreateUser(context.Background(), &client.UserCreate{
-		Company:   company,
-		Email:     email,
-		FirstName: firstName,
-		LastName:  lastName,
-		Password:  password,
-	})
-	if err != nil {
-		t.Fatalf("Failed to created user: %s", err.Error())
-	}
-	if user == nil {
-		t.Error("user is nil")
-	}
-}
+// TODO: Enable when temp email integrated
+// func TestCreateUser(t *testing.T) {
+// 	email = fmt.Sprintf("gotest%s@testworx.io", randomString(7))
+// 	password = os.Getenv("USER_PW")
+// 	if password == "" {
+// 		t.Fatal("Environment variable USER_PW is not set")
+// 	}
+// 	user, err := testClient.CreateUser(context.Background(), &client.UserCreate{
+// 		Company:   company,
+// 		Email:     email,
+// 		FirstName: firstName,
+// 		LastName:  lastName,
+// 		Password:  password,
+// 	})
+// 	if err != nil {
+// 		t.Fatalf("Failed to created user: %s", err.Error())
+// 	}
+// 	if user == nil {
+// 		t.Error("user is nil")
+// 	}
+// }
 
 func TestLogin(t *testing.T) {
+	email = os.Getenv("USER_EMAIL")
+ 	if email == "" {
+ 		t.Fatal("USER_EMAIL is not set")
+ 	}
+ 	password = os.Getenv("USER_PW")
+ 	if password == "" {
+ 		t.Fatal("USER_PW is not set")
+ 	}
 	err := testClient.Login(context.Background(), &client.LoginRequest{
 		Email:    email,
 		Password: password,
@@ -82,11 +97,85 @@ func TestLogin(t *testing.T) {
 }
 
 func TestCreateProject(t *testing.T) {
+	project = randomString(10)
 	proj, err := testClient.CreateProject(context.Background(), project)
 	if err != nil {
-		t.Errorf("Failed to created project: %s", err.Error())
+		t.Fatalf("Failed to created project: %s", err.Error())
 	}
 	if proj == nil {
-		t.Error("proj is nil")
+		t.Fatal("proj is nil")
 	}
+	projectID = proj.ID
+}
+
+func TestCreateRBACResources(t *testing.T) {
+	// Registration Token
+	tokenReq := client.RegistrationToken{
+		Name:             randomString(10),
+		Description:      "default",
+	}
+	tokenResp, err := testClient.CreateRegistrationToken(context.Background(), projectID, tokenReq)
+	if err != nil {
+		t.Errorf("Failed to create registration token: %s", err.Error())
+		return
+	}
+	if tokenResp == nil {
+		t.Errorf("tokenResp is nil")
+		return
+	}
+	// Service Account
+	svcAccReq := client.ServiceAccountCreate{
+		Name:        randomString(10),
+		Description: "default",
+	}
+	svcAccResp, err := testClient.CreateServiceAccount(context.Background(), projectID, svcAccReq)
+	if err != nil {
+		t.Errorf("Failed to create service account: %s", err.Error())
+		return
+	}
+	if svcAccResp == nil {
+		t.Errorf("svcAccResp is nil")
+		return
+	}
+	// Role
+	roleReq := &client.RoleCreate{
+		Name:        randomString(10),
+		Config:      roleYAML,
+	}
+	roleResp, err := testClient.CreateRole(context.Background(), projectID, roleReq)
+	if err != nil {
+		t.Errorf("Failed to create role: %s", err.Error())
+		return
+	}
+	if roleResp == nil {
+		t.Errorf("roleResp is nil")
+		return
+	}
+	// Role binding
+	roleBindingResp, err := testClient.CreateRoleBinding(context.Background(), projectID, svcAccResp.ID, roleResp.ID)
+	if err != nil {
+		t.Errorf("Failed to create role binding: %s", err.Error())
+		return
+	}
+	if roleBindingResp == nil {
+		t.Error("roleBindingResp is nil")
+		return
+	}
+	// Access Key
+	accessKeyResp, err := testClient.CreateAccessKey(context.Background(), projectID, svcAccResp.ID)
+	if err != nil {
+		t.Errorf("Failed to create access key: %s", err.Error())
+		return
+	}
+	if accessKeyResp == nil {
+		t.Error("accessKeyResp is nil")
+		return
+	}
+}
+
+func TestDeleteProject(t *testing.T) {
+	 err := testClient.DeleteProject(context.Background(), projectID)
+	 if err != nil {
+		 t.Errorf("Failed to delete project: %s", err.Error())
+	 }
 }
